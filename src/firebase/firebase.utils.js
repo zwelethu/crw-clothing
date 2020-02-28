a26d12f18bfdb0a77139b106ab8fd8e8a8289e8d
zwelethu <zwelethu@yahoo.com>
2020-02-29 01:36:43 +0200

    Added email and password sign-up

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,26 @@ const config = {
   appId: '1:670563453063:web:158a67e03fefee89c51022',
   measurementId: 'G-6GYDR02LL8'
 };
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const snapShot = await userRef.get();
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (err) {
+      console.log('error creating user.', err.message);
+    }
+  }
+  return userRef;
+};
 
 firebase.initializeApp(config);
 
