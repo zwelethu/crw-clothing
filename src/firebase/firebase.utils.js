9d4b96005b1468110b7b2ca5c3daa6d7577b1203
zwelethu <zwelethu@yahoo.com>
2020-03-04 22:37:50 +0200

    added redux

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,9 @@ const config = {
   appId: '1:670563453063:web:158a67e03fefee89c51022',
   measurementId: 'G-6GYDR02LL8'
 };
+
+firebase.initializeApp(config);
+
 export const createUserProfileDocument = async (userAuth, additionalData) => {
   if (!userAuth) return;
   const userRef = firestore.doc(`users/${userAuth.uid}`);
@@ -32,8 +35,6 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
   }
   return userRef;
 };
-
-firebase.initializeApp(config);
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
